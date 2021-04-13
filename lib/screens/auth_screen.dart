252f6f03736a71766f07e3b600da2b5a100434e9
Victor <vcolofati@gmail.com>
2021-04-13 10:51:48 -0300

    Integration with Firebase Auth

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,6 +1,8 @@
 import 'package:chat/models/auth_data.dart';
 import 'package:chat/widgets/auth_form.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AuthScreen extends StatefulWidget {
   @override
@@ -8,9 +10,31 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  void _handleSubmit(AuthData authData) {
-    print(authData.getName);
-    print(authData.getEmail);
+  final _auth = FirebaseAuth.instance;
+  Future<void> _handleSubmit(AuthData authData) async {
+    try {
+      if (authData.isLogin) {
+        await _auth.signInWithEmailAndPassword(
+          email: authData.getEmail!.trim(),
+          password: authData.getPassword!,
+        );
+      } else {
+        await _auth.createUserWithEmailAndPassword(
+          email: authData.getEmail!.trim(),
+          password: authData.getPassword!,
+        );
+      }
+    } on FirebaseAuthException catch (err) {
+      final msg = err.message ?? 'Occurred an error. Check your credentials!';
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(msg),
+          backgroundColor: Theme.of(context).errorColor,
+        ),
+      );
+    } catch (err) {
+      print(err);
+    }
   }
 
   @override
