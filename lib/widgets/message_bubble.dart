77809f4c7f976a58c9bf97bee30c926fae0b85ad
Victor <vcolofati@gmail.com>
2021-04-13 14:17:51 -0300

    Message widget improved

---
+++ lib/widgets/message_bubble.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  final Key? key;
+  final String message;
+  final bool belongsToMe;
+
+  MessageBubble(this.message, this.belongsToMe, {this.key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment:
+          belongsToMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color:
+                belongsToMe ? Colors.grey[300] : Theme.of(context).accentColor,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12),
+              topRight: Radius.circular(12),
+              bottomLeft:
+                  belongsToMe ? Radius.circular(12) : Radius.circular(0),
+              bottomRight:
+                  belongsToMe ? Radius.circular(0) : Radius.circular(12),
+            ),
+          ),
+          width: 140,
+          margin: const EdgeInsets.symmetric(
+            vertical: 4,
+            horizontal: 8,
+          ),
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 16,
+          ),
+          child: Text(
+            message,
+            style: TextStyle(
+              color: belongsToMe
+                  ? Colors.black
+                  : Theme.of(context).accentTextTheme.headline1?.color,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/widgets/messages.dart
+++ lib/widgets/messages.dart
@@ -1,9 +1,12 @@
+import 'package:chat/widgets/message_bubble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    User? user = FirebaseAuth.instance.currentUser;
     CollectionReference chats = FirebaseFirestore.instance.collection('chat');
     return StreamBuilder<QuerySnapshot>(
       stream: chats.orderBy('createdAt', descending: true).snapshots(),
@@ -17,8 +20,10 @@ class Messages extends StatelessWidget {
         return ListView.builder(
           reverse: true,
           itemCount: chatDocs?.length,
-          itemBuilder: (ctx, i) => Text(
+          itemBuilder: (ctx, i) => MessageBubble(
             chatDocs?[i]['text'],
+            chatDocs?[i]['userId'] == user?.uid,
+            key: ValueKey(chatDocs?[i].id),
           ),
         );
       },
