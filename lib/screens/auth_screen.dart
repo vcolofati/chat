4de39f1255adf7d02590f95fb458fb3eb8ec8a59
Victor <vcolofati@gmail.com>
2021-04-13 11:22:35 -0300

    Implements AuthScreen, fix error initializing Firebase core

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,8 +1,8 @@
 import 'package:chat/models/auth_data.dart';
 import 'package:chat/widgets/auth_form.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class AuthScreen extends StatefulWidget {
   @override
@@ -11,18 +11,36 @@ class AuthScreen extends StatefulWidget {
 
 class _AuthScreenState extends State<AuthScreen> {
   final _auth = FirebaseAuth.instance;
+  bool _isLoading = false;
+
   Future<void> _handleSubmit(AuthData authData) async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    UserCredential userCredential;
+
     try {
       if (authData.isLogin) {
-        await _auth.signInWithEmailAndPassword(
+        userCredential = await _auth.signInWithEmailAndPassword(
           email: authData.getEmail!.trim(),
           password: authData.getPassword!,
         );
       } else {
-        await _auth.createUserWithEmailAndPassword(
+        userCredential = await _auth.createUserWithEmailAndPassword(
           email: authData.getEmail!.trim(),
           password: authData.getPassword!,
         );
+
+        final userData = {
+          'name': authData.getName,
+          'email': authData.getEmail,
+        };
+
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userCredential.user?.uid)
+            .set(userData);
       }
     } on FirebaseAuthException catch (err) {
       final msg = err.message ?? 'Occurred an error. Check your credentials!';
@@ -34,6 +52,10 @@ class _AuthScreenState extends State<AuthScreen> {
       );
     } catch (err) {
       print(err);
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
     }
   }
 
@@ -41,7 +63,29 @@ class _AuthScreenState extends State<AuthScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: AuthForm(_handleSubmit),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Stack(
+              children: [
+                AuthForm(_handleSubmit),
+                if (_isLoading)
+                  Positioned.fill(
+                    child: Container(
+                      margin: const EdgeInsets.all(20),
+                      decoration: const BoxDecoration(
+                          color: Color.fromRGBO(0, 0, 0, 0.5)),
+                      child: const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    ),
+                  )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
